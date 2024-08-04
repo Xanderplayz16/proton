7dc46f8305d0c22a2dce69efd87755678338aa6f
xander mckay <xande@noyoudon.t>
2024-08-04 16:55:13 -0400

    Finally fix test.yml

--- proton/pyhtml/generate_globals.py
+++ proton/pyhtml/generate_globals.py
@@ -1,7 +1,7 @@
 from .document import Document
 
 def generateGlobals(self):
-    alert = lambda message: self.webview.evaluate_js(f"alert({'"'}{message}{'"'})")
+    alert = lambda message: self.webview.evaluate_js(f'alert("{message}")')
     document = Document(self)
     return {"alert": alert, "document": document, "window": self}
 
