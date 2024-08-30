357ccf377f6e0e1bc31ba967e7c0514e1589fab3
xander mckay <xande@noyoudon.t>
2024-08-30 19:24:23 -0400

    Added project config file

--- proton/__main__.py
+++ proton/__main__.py
@@ -1,5 +1,5 @@
 import rich.style
-import typer, os, platform, shutil, rich, sys
+import typer, os, platform, shutil, rich, sys, toml
 from rich.progress import Progress, SpinnerColumn, TextColumn
 import subprocess as sp
 from rich.panel import Panel
@@ -35,12 +35,30 @@ def init(dir:str="."):
 
 
 @projectapp.command()
-def build(disable_qt: bool = True, disable_gtk: bool = False, verbose: bool = False, disable_console: bool = True, enable_experimental_bloat_removal: bool = False):
+def build():
     """Build your project."""
     with Progress(SpinnerColumn(), TextColumn("[progress.description]{task.description}"), transient=True) as progress:
+        with open('project.toml', 'r') as f:
+            cfg = toml.load(f)
+        try: experimental_bloat_removal = cfg["proton"]["build"]["bloat"]["experimental_bloat_removal"]
+        except Exception: experimental_bloat_removal = False
+        try: disable_qt = cfg["proton"]["build"]["bloat"]["disable_qt"]
+        except Exception: disable_qt = True
+        try: disable_gtk = cfg["proton"]["build"]["bloat"]["disable_gtk"]
+        except Exception: disable_gtk = True
+        try: verbose = cfg["proton"]["build"]["verbose"]
+        except Exception: verbose = False
+        try: entrypoint = cfg["proton"]["entrypoint"]
+        except Exception: print('Please set entrypoint in project.toml'); exit()
+        try: datafiles = cfg["proton"]["build"]["datafiles"]
+        except Exception: datafiles = []
+        try: windows_disable_console = cfg["proton"]["build"]["windows_disable_console"]
+        except Exception: windows_disable_console = False
         extra_args = ""
-        if enable_experimental_bloat_removal:
+        if experimental_bloat_removal:
             print('WARNING: You are using experimental bloat removal, and this may cause all kinds of issues.')
+        if experimental_bloat_removal and (os.name == 'nt'):
+            error('Experimental bloat removal does nothing on windows, as there aren\'t too many unnessecary libraries that can be removed.'); exit()
         if disable_qt:
             extra_args += '--nofollow-import-to=PySide6 --nofollow-import-to=qtpy --nofollow-import-to=PySide2 '
         else:
@@ -49,7 +67,7 @@ def build(disable_qt: bool = True, disable_gtk: bool = False, verbose: bool = Fa
             extra_args += '--nofollow-import-to=gi '
         if verbose:
             extra_args += '--verbose '
-        if disable_console and os.name == 'nt': # Disabling the console does nothing on non-Windows systems.
+        if windows_disable_console and os.name == 'nt': # Disabling the console does nothing on non-Windows systems.
             extra_args += '--disable-console '
         if platform.system() == "Darwin":
             # macos
@@ -63,9 +81,9 @@ def build(disable_qt: bool = True, disable_gtk: bool = False, verbose: bool = Fa
         print("")
         
         
-        p = sp.Popen(f"python -m nuitka src/main.py --standalone --nofollow-import-to=cefpython3 --nofollow-import-to=kivy --nofollow-import-to=jnius --nofollow-import-to=PyQt5 {extra_args} 2>&1", shell=True, stdout = sp.PIPE, stderr = sp.DEVNULL)
+        p = sp.Popen(f"python -m nuitka {entrypoint} --standalone --nofollow-import-to=cefpython3 --nofollow-import-to=kivy --nofollow-import-to=jnius --nofollow-import-to=PyQt5 {extra_args} 2>&1", shell=True, stdout = sp.PIPE, stderr = sp.DEVNULL)
         prev = b''
-        while p.poll() == None:
+        while p.poll() == None: # TODO: find out why this doesn't work on windows
             a = p.stdout.read(1)
             prev = prev+a
             if a == b'\n':
@@ -86,13 +104,15 @@ def build(disable_qt: bool = True, disable_gtk: bool = False, verbose: bool = Fa
             os.mkdir("dist")
             app = [i for i in os.listdir if i.endswith(".app")][0]
             shutil.copytree(app, os.path.join('dist', app))
-            shutil.copytree('web', os.path.join('dist', app, 'Contents', 'MacOS', 'web'))
+            for i in datafiles:
+                shutil.copytree({i}, os.path.join('dist', app, 'Contents', 'MacOS', {i}))
             shutil.rmtree(app)
         else:
             shutil.move("main.dist", "dist")
-            shutil.copytree("web", "dist/web")
-
-        if enable_experimental_bloat_removal:
+            for i in datafiles:
+                shutil.copytree(i, os.path.join('dist', i))
+        
+        if experimental_bloat_removal:
             files = "libncursesw.so.6 libssl.so.3 libuuid.so.1 libbz2.so.1.0 libgcc_s.so.1 libcrypto.so.3 libpcre2-8.so.0 gevent girepository greenlet cryptography _brotli.so libglib-2.0.so.0"
             for i in files.split(' '):
                 try:
